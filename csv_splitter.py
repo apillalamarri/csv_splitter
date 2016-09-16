1aa0838b1a88f3f514ac9b0dd25047e958096132
apillalamarri <anupama.pillalamarri@gmail.com>
2016-09-16 13:44:34 -0400

    Removes instructions from script
    Because they are now in the README

--- csv_splitter.py
+++ csv_splitter.py
@@ -1,22 +1,3 @@
-#This script splits a csv into chunks of a given number of rows each,
-#each with the header from the original csv
-#
-#To run it, type:
-#python csv_splitter.py <csv filename> <number of rows per chunk>
-#on the command line
-#
-#Suppose big_file.csv has 100000 rows. Typing:
-#
-#python csv_splitter.py big_file.csv 30000
-#
-#will create four files called:
-#big_file_part_1.csv
-#big_file_part_2.csv
-#big_file_part_3.csv
-#big_file_part_4.csv
-#
-#in the same directory as csv_splitter.py and big_file.csv
-
 import sys
 import csv
 
